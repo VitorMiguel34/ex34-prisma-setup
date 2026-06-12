497db293e56c3f071b38d13d4b93cd88f4d8e7ba
Victor Miguel da Silva Bezerra <miguelvictor035@gmail.com>
2026-06-12 19:25:02 -0300

    Update server.js

--- server.js
+++ server.js
@@ -7,4 +7,9 @@ app.use(express.json());
 // TODO: GET /health → 200 { db: "ok" }
 // Confirme a conexão com o banco usando prisma.$queryRaw`SELECT 1`
 
+app.get("/health", (req,res) => {
+  prisma.$queryRaw`SELECT 1`
+  res.status(200).json({db: "ok"})
+})
+
 app.listen(3000, () => console.log('Servidor rodando em http://localhost:3000'));
